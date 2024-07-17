bbd87e9460dc37696120243f9c3d42bbce212a18
Nicolas Ricaldi <36467209+nricaldi@users.noreply.github.com>
2024-07-17 16:46:55 -0400

    ignoring python venv directory when using telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,9 @@
 
 require('telescope').setup{
   defaults = {
+    file_ignore_patterns = {
+      "venv"
+    },
     -- Default configuration for telescope goes here:
     -- config_key = value,
     mappings = {
